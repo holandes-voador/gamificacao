6225cee7bbfc759ef865c012b9a157674c68b3ed
matheusbsilva <matheusbattista@hotmail.com>
2018-12-03 21:01:06 -0300

    setup comment creation on notice and event

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,32 @@
 class CommentsController < ApplicationController
+  def create
+    @user = current_user
+
+    if params[:event_id]
+      @item = Event.find_by(id: params[:event_id])
+      @comment = Comment.new(event: @item)
+    elsif params[:notice_id]
+      @item = Notice.find_by(id: params[:notice_id])
+      @comment = Comment.new(notice: @item)
+    else
+      raise ArgumentError, 'No id for event or notice provided'
+    end
+
+    @comment.user = @user
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @item, notice: 'Comment was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+
+    if @comment.save!
+
+    else
+
+    end
+  end
+
 end
