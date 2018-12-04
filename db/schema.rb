a008034830204b17125bc07c3244771ffb74eb92
matheusbsilva <matheusbattista@hotmail.com>
2018-12-04 00:53:26 -0300

    add cost to reward

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_04_032617) do
+ActiveRecord::Schema.define(version: 2018_12_04_034949) do
 
   create_table "checkins", force: :cascade do |t|
     t.integer "user_id"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2018_12_04_032617) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "cost"
   end
 
   create_table "users", force: :cascade do |t|
